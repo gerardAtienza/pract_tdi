c778493b6baac91db7343d73e2f81af47878586e
Gerard <gerix.ati@gmail.com>
2023-11-14 12:01:33 +0100

    sqx

--- controlador/model/categories.php
+++ controlador/model/categories.php
@@ -1,15 +1,14 @@
 <?php
 
 function getCategories($conn){
-    // Pas 1: preparem la consulta.
+    // Preparem la consulta.
     $sql = 'SELECT id_categoria, `nom`
     FROM categoria';
 
-    // Pas 2: Enviem la query a la BBDD. La variable $conn
-    // és la definida al pas anterior.
+    // Enviem la query a la BBDD. La variable $conn
     $result = pg_query($conn, $sql);
 
-    // Pas 3: agafem els resultats de la consulta.
+    // Agafem els resultats de la consulta.
     $array_elements = pg_fetch_all($result);
     return $array_elements;
 }
--- controlador/model/productes.php
+++ controlador/model/productes.php
@@ -1,15 +1,14 @@
 <?php
 
 function getProductes($conn){
-    // Pas 1: preparem la consulta.
+    // Preparem la consulta.
     $sql = 'SELECT id_prod, `nom`
-    FROM producte';
+    FROM producte';  // Modificar perque nomes mostri els resultats que tinguin un id_categoria en concret
 
-    // Pas 2: Enviem la query a la BBDD. La variable $conn
-    // és la definida al pas anterior.
+    //Enviem la query a la BBDD. La variable $conn
     $result = pg_query($conn, $sql);
 
-    // Pas 3: agafem els resultats de la consulta.
+    // Agafem els resultats de la consulta.
     $array_elements = pg_fetch_all($result);
     return $array_elements;
 }
