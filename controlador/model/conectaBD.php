37d23b8cfab98fa7e239d1cf0e8f66f8caf56ba5
Santi <esoftware2023uab@gmail.com>
2023-11-14 13:17:31 +0100

    j

--- controlador/model/conectaBD.php
+++ controlador/model/conectaBD.php
@@ -3,8 +3,9 @@
     $databaseUser = 'tdiw-l6';
     $databasePassword = 'CLVcSyGg';
     $databaseHost = '127.0.0.1';
+    $port = '22';
 
-    $conn = pg_connect("dbname=$databaseName user=$databaseUser password=$databasePassword host=$databaseHost");
+    $conn = pg_connect("dbname=$databaseName user=$databaseUser password=$databasePassword host=$databaseHost port=$port");
     if (!$conn) {
         echo "Failed to connect to the database";
     }
